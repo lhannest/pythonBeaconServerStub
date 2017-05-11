8b7d9dabc9de12212fd778efb3dc3f852847bbb3
lhannest <lhannest@gmail.com>
2017-05-11 07:58:12 -0700

    modified

--- py3server/swagger_server/controller_impl.py
+++ py3server/swagger_server/controller_impl.py
@@ -1,9 +1,9 @@
 from swagger_server.models import InlineResponse2003, StatementsSubject, StatementsObject, StatementsPredicate
 
 def getStatementResponse(c):
-    s = StatementsObject('123', 'Lance')
-    p = StatementsPredicate('456', 'is cooler than')
-    o = StatementsObject('789', 'nearly everyone else')
+    s = StatementsObject('123', 'concept A')
+    p = StatementsPredicate('456', 'relationship')
+    o = StatementsObject('789', 'concept B')
 
     responses = [InlineResponse2003(curie, s, p, o) for curie in c]
 
